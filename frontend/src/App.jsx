3ae10b241cf6357e5866e7f35a5505bc88a4ba8f
Arushi2505 <arushi1625@gmail.com>
2026-06-16 20:01:49 +0530

    Replace localhost with Render backend URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -32,7 +32,7 @@ function App() {
 
     try {
       const response = await axios.post(
-        "http://127.0.0.1:8000/analyze",
+        "https://ai-resume-analyzer-rvoq.onrender.com/analyze",
         formData
       );
 
